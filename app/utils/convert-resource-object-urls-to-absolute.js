7bff6082ec22b760bb6d3b1f36cce8074c069391
Mark Hendrickson <markmhendrickson@gmail.com>
2018-01-26 19:42:17 +0100

    Add imgix CDN support for image URLs

--- app/utils/convert-resource-object-urls-to-absolute.js
+++ app/utils/convert-resource-object-urls-to-absolute.js
@@ -1,5 +1,15 @@
 var _ = require('underscore'),
+  imgix,
+  ImgixClient = require('imgix-core-js'),
+  isImage = require('is-image'),
   path = require('path');
+
+if (process.env.PERSONAL_WEB_IMGIX_HOST && process.env.PERSONAL_WEB_IMGIX_SECURITY_TOKEN) {
+  imgix = new ImgixClient({
+    host: process.env.PERSONAL_WEB_IMGIX_HOST,
+    secureURLToken: process.env.PERSONAL_WEB_IMGIX_SECURITY_TOKEN
+  });
+}
 
 module.exports = function(resourceObject, req) {
   if (!resourceObject || !req) { return; }
@@ -12,8 +22,37 @@ module.exports = function(resourceObject, req) {
     return (typeof resourceObject.id === 'string' && resourceObject.id.indexOf('Url') === (resourceObject.id.length - 3) && key === 'value');
   };
 
-  resourceObject.attributes = _.mapObject(resourceObject.attributes, function(val, key) {
-    return (isRelativeUrlProperty(key, val) || isRelativeUrlItemValue(resourceObject, key, val)) ? 'http://' + path.join(req.headers.host, val) : val;
+  Object.keys(resourceObject.attributes).forEach((key) => {
+    var val = resourceObject.attributes[key];
+
+    if (isRelativeUrlProperty(key, val) || isRelativeUrlItemValue(resourceObject, key, val)) {
+      var url = 'http://' + path.join(req.headers.host, val);
+
+      if (imgix) {
+        if (req.headers.host.indexOf('127.0.0.1') === 0 && process.env.PERSONAL_WEB_IMGIX_DEPLOY_HOST) {
+          url = 'http://' + path.join(process.env.PERSONAL_WEB_IMGIX_DEPLOY_HOST, val);
+        }
+        
+        resourceObject.attributes[key] = imgix.buildURL(url);
+
+        if (isImage(val)) {
+          resourceObject.attributes[`thumb-${key}`] = imgix.buildURL(resourceObject.attributes[key], {
+            crop: 'focalpoint',
+            fit: 'crop',
+            'max-h': 800,
+            'max-w': 800
+          });
+
+          resourceObject.attributes[`large-${key}`] = imgix.buildURL(resourceObject.attributes[key], {
+            fit: 'max',
+            h: 2000,
+            w: 2000
+          });
+        }
+      } else {
+        resourceObject.attributes[key] = url;
+      }
+    }
   });
 
   return resourceObject;
